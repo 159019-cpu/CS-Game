81fddd06bae864c591ecea616ddd56c146601e0f
151879-tech <151879@ishweb.nl>
2025-12-17 12:15:47 +0100

    Update Team_A.py

--- Team_A.py
+++ Team_A.py
@@ -1,1 +1,14 @@
+#This is the base
+import random
+DAY = 1
+health = 100
+warmth = 100
+hunger = 100
+print ("you have 4 options")
+print ("click 1 for rest")
+print("click 2 for gathering")
 
+choice = input("choose")
+
+if choice == 1:
+    health = health - random.randint(10,15)
